492ac686289e8e4f393e28b422ef4c8473840824
Raymond Barlow <rbarlow@raymanoz.com>
2016-02-16 22:10:12 +1000

    added last & last_option

--- lib/sequence.rb
+++ lib/sequence.rb
@@ -8,13 +8,15 @@ module Sequences
     if items.first.nil?
       empty
     else
-      Sequence.new(*items)
+      Sequence.new(items)
     end
   end
 
   class Sequence
-    def initialize(*items)
-      @enumeratable = items
+    include Comparable
+
+    def initialize(enumeratable)
+      @enumeratable = enumeratable
     end
 
     def head
@@ -23,19 +25,61 @@ module Sequences
 
     def head_option
       begin
-        some(enumerator.peek)
-      rescue
+        some(head)
+      rescue StopIteration
         none
       end
     end
 
-    private
+    def last
+      reverse.head
+    end
+
+    def last_option
+      reverse.head_option
+    end
+
+    def reverse
+      sequence(*@enumeratable.reverse)
+    end
+
     def enumerator
       @enumeratable.each
+    end
+
+    private
+    def <=>(other)
+      a_enumerator = enumerator
+      b_enumerator = other.enumerator
+
+      compare = 0
+      while has_next(a_enumerator) && has_next(b_enumerator)
+        a = a_enumerator.next
+        b = b_enumerator.next
+        compare = a<=>(b)
+        if compare != 0
+          return compare
+        end
+      end
+
+      if !(has_next(a_enumerator) || has_next(b_enumerator))
+        compare
+      else
+        has_next(a_enumerator) ? 1 : -1
+      end
+    end
+
+    def has_next(enumerator)
+      begin
+        enumerator.peek
+        true
+      rescue StopIteration
+        false
+      end
     end
   end
 
   private
 
-  EMPTY=Sequence.new
+  EMPTY=Sequence.new([])
 end
--- spec/sequence_spec.rb
+++ spec/sequence_spec.rb
@@ -9,9 +9,26 @@ describe 'Sequence' do
     expect(sequence(1,2).head).to eq(1)
   end
 
+  it 'should return same result when head called many times' do
+    expect(sequence(1,2).head).to eq(1)
+    expect(sequence(1,2).head).to eq(1)
+  end
+
   it 'should support head_option' do
     expect(sequence(1).head_option).to eq(some(1))
     expect(empty.head_option).to eq(none)
   end
 
+  it 'should support reverse' do
+    expect(sequence(1,2,3).reverse).to eq(sequence(3,2,1))
+  end
+
+  it 'should support last' do
+    expect(sequence(1,2,3).last).to eq(3)
+  end
+
+  it 'should support last_option' do
+    expect(sequence(1,2,3).last_option).to eq(some(3))
+    expect(empty.last_option).to eq(none)
+  end
 end
